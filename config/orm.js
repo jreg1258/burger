376a525365ab3a1476272751c987b6d372bb1b1d
Joey Register <josephregister1258@gmail.com>
2020-02-19 20:29:37 -0500

    created orm

--- config/orm.js
+++ config/orm.js
@@ -1,14 +1,29 @@
 const connection = require("./connection")
 
+const tableName = "burgers"
+
 const orm = {
     selectAll: (callback) => {
-        const query = "SELECT * FROM" + tableName
+        const query = "SELECT * FROM " + tableName
 
         connection.query(query, (err, result)=>{
             if (err) throw err;
             callback(result)
         })},
-    selectOne: (callback) => {
-        const query = "SELECT ?? FROM ?? WHERE id = ?"
+    selectOne: (name,callback) => {
+        const query = "SELECT * FROM "+tableName+" WHERE id = ?"
+
+        connection.query(query, [name], (err,result)=>{
+            if (err) throw err;
+            callback(result)
+        })},
+    updateOne: (burger, id, callback) => {
+        const col = "burger_name"
+        const query = "UPDATE "+tableName+" SET "+col+"="+burger+" WHERE id = ?;"
+
+        connection.query(query, [id], (err, result)=>{
+            if (err) throw err;
+            callback(result)
+        })
     }
 }
