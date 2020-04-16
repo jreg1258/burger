829d79eb64874d5ac262e700f70abe31bf685296
Joey Register <josephregister1258@gmail.com>
2020-04-16 12:39:12 -0400

    added styling

--- config/orm.js
+++ config/orm.js
@@ -50,8 +50,6 @@ var orm = {
     queryString += printQuestionMarks(vals.length);
     queryString += ") ";
 
-    console.log(queryString);
-
     connection.query(queryString, vals, function(err, result) {
       if (err) {
         throw err;
@@ -68,7 +66,6 @@ var orm = {
     queryString += " WHERE ";
     queryString += condition;
 
-    console.log(queryString);
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
